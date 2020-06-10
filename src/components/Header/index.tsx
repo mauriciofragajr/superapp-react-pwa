86502300b81ce1b9c2b7efe821fd5d198107cffa
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2020-06-09 21:50:17 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      Super App
+    </Container>
+  )
+}
+
+export default Header
